53dd88d49e2d3b3e2d823784e0f78b996a9741d9
Changnam Go <changnam.go@gmail.com>
2025-02-11 23:11:15 +0900

    Counter 를 root 에서 제외하고 about 에만 뒀는데도, 최초 요청에 bundle 되어 들어 왔음. /about 호출시 (client navigation) 에 따로 받는것 없음

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import Link from "next/link";
-import Counter from "@/component/Counter";
+// import Counter from "@/component/Counter";
 
 export default function Home() {
   return (
     <>
     <Link href="/about">about</Link>
-    <Counter />
+    {/*<Counter />*/}
     </>
   );
 }
