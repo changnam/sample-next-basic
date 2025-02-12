b3bd7a83104e3afb258d42a485586333dd57cb65
Changnam Go <changnam.go@gmail.com>
2025-02-12 20:25:16 +0900

    SSR 은 build 시에 route component 와 import 된 child component 에 대해서만 렌더링함. 사용하지 않는 component 는 SSR 하지 않음

---
+++ component/ClientComponent.jsx
@@ -0,0 +1,7 @@
+'use client';
+
+export default function ClientComponent(){
+    return (
+        <div>클라이언트 컴포넌트</div>
+    )
+}
---
+++ component/ServerComponent.jsx
@@ -0,0 +1,5 @@
+export default function ServerComponent(){
+    return (
+        <div>서버 컴포넌트트</div>
+    )
+}
