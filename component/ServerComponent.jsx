44750159f7fd4fa39140948d31b4f8331491641d
Changnam Go <changnam.go@gmail.com>
2025-02-12 21:00:05 +0900

    server component 에서 fetch 된 데이터는 .next/cache 에 적재되어 재사용됨. /osts/1 을 빌드시에 두번 호출하는데, 실제로 api 서버로는 1번만 request가 요청됨

--- component/ServerComponent.jsx
+++ component/ServerComponent.jsx
@@ -1,5 +1,9 @@
-export default function ServerComponent(){
+export default async function ServerComponent(){
+    const data = await fetch(`http://localhost:4000/api/posts/1`).then((res) =>
+        res.json()
+      );
+
     return (
-        <div>서버 컴포넌트트</div>
+        <div>서버 컴포넌트 {data.title}</div>
     )
 }
